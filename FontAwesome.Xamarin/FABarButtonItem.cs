dd17375c31a0336de0c531e4a03f3d8c1f4345ac
Neil Kennedy <neil.p.kennedy@hotmail.com>
2014-03-02 19:19:01 +0000

    Added FABarButtonItem
    Added FABarButtonItem class as a FontAwesome extension of
    UIBarButtonItem.

---
+++ FontAwesome.Xamarin/FABarButtonItem.cs
@@ -0,0 +1,52 @@
+﻿//
+// FABarButtonItem.cs
+//
+// Author:
+//       Neil Kennedy <neil.p.kennedy@outlook.com>
+//
+// Copyright (c) 2014 neilkennedy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace FontAwesomeXamarin
+{
+	public class FABarButtonItem : UIBarButtonItem
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FontAwesome.Xamarin.FABarButtonItem"/> class.
+		/// Use the CustomView property to access the button that we create with the new icon
+		/// </summary>
+		/// <param name="icon">An icon from <see cref="FontAwesome.Xamarin.FontAwesome"/></param>
+		/// <param name="handler">Event handler to be used on click</param>
+		public FABarButtonItem (string icon, UIColor fontColor, EventHandler handler) : base()
+		{
+			var iconButton = new UIButton (new RectangleF (0, 0, 32, 32)) {
+				Font = FontAwesome.Font (25)
+			};
+			iconButton.SetTitle (icon, UIControlState.Normal);
+			iconButton.SetTitleColor (fontColor, UIControlState.Normal);
+			iconButton.TouchUpInside += handler;
+
+			CustomView = iconButton;
+		}
+	}
+}
--- readme.markdown
+++ readme.markdown
@@ -2,7 +2,7 @@
 
 This is a simple library to help when using [FontAwesome](http://fortawesome.github.io/Font-Awesome/) with Xamarin.iOS. All v4.0.3 icons from the [cheatsheet](http://fortawesome.github.io/Font-Awesome/cheatsheet/) are included.
 
-####Getting started
+##Getting started
 
 - Add dist/FontAwesome.Xamarin.dll to your project
 - Add dist/FontAwesome.ttf to the Resources directory of your project
@@ -13,7 +13,8 @@ This is a simple library to help when using [FontAwesome](http://fortawesome.git
     - Add a new property and select "Fonts provided by application" from the dropdown list
     - Add a new item to the array called FontAwesome.ttf
     
-####Using it
+##Using it
+###FontAwesome.cs
 Once the library and font have been added, using FontAwesome is very simple. There is one static method `.Font(int size)` that returns the `UIFont` class with the specified size. Each icon is listed as a static method. The CSS class of `fa-lock` would become `FontAwesome.FALock`.
 
 ```csharp
@@ -21,10 +22,19 @@ using FontAwesomeXamarin;
 
 UILabel lockLabel = new UILabel (new RectangleF (0, 30, 320, 100)) {
     Font = FontAwesome.Font(100), //100 is the font size we want to use
-    Text = FontAwesome.FALock, //Here's we're using the lock icon "fa-lock"
+    Text = FontAwesome.FALock, //Here we're using the lock icon "fa-lock"
     TextColor = UIColor.White,
     TextAlignment = UITextAlignment.Center
 };
 ```
 
 Because FontAwesome is a font based icon library we can set the text color and size in the same way we would for any other regular font.
+
+###FABarButtonItem.cs
+This will create a `UIBarButtonItem` with a FontAwesome icon that can be used in a `UINavigationBar`. Internally it uses a `UIButton` for the font and click event and adds it as the `CustomView` of the `UIBarButtonItem`.
+
+```csharp
+NavigationItem.RightBarButtonItem = new FABarButtonItem (FontAwesome.FATrashO, UIColor.White, delegate {
+    //Click event handler
+});
+```
