06bd548ae84df0f37ceb6794243918a514349b2e
Neil Kennedy <neil.p.kennedy@hotmail.com>
2014-03-02 14:20:08 +0000

    Initial Commit
    First commit. Basic functionality but it works. More to come.

---
+++ generator.js
@@ -0,0 +1,46 @@
+/*
+    Use this JS snippet to get the public string properties of all the FontAwesome icons.
+    
+    Use Chrome dev tools to run the below snippet into http://fortawesome.github.io/Font-Awesome/cheatsheet/
+    
+    Call fontAwesomeXamarin.generate() to print all the icon properties to the dev console.
+
+*/
+var FontAwesomeXamarin = {
+
+    columns: $('.col-md-4.col-sm-6.col-lg-3').clone(),
+
+    generate: function () {
+        var self = this,
+            label,
+            unicode,
+            spanElement,
+            iElement;
+        $.each(self.columns, function (index, column) {
+            column = $(column);
+            spanElement = column.find('span');
+            iElement = column.find('i');
+
+            unicode = spanElement.text().replace("(&#x", "\\u").replace(";)", "");
+
+            spanElement.remove();
+            iElement.remove();
+
+            label = self.toTitleCase(column.text());
+            label = label.replace("Fa", "FA").replace(/- */g, ""); //remove the dashes and capitalise the Fa
+
+            console.log("public static string " + label + " = \"" + unicode + "\";");
+
+        });
+    },
+
+    toTitleCase: function (str) {
+        str = str.replace(/^\s+|\s+$/g, ""); //trim whitespace
+
+        return str.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+};
+
+window.fontAwesomeXamarin = FontAwesomeXamarin;
---
+++ readme.markdown
@@ -0,0 +1,30 @@
+#FontAwesome.Xamarin
+
+This is a simple library to help when using [FontAwesome](http://fortawesome.github.io/Font-Awesome/) with Xamarin.iOS. All v4.0.3 icons from the [cheatsheet](http://fortawesome.github.io/Font-Awesome/cheatsheet/) are included.
+
+####Getting started
+
+- Add dist/FontAwesome.Xamarin.dll to your project
+- Add dist/FontAwesome.ttf to the Resources directory of your project
+    - Set Build Action to "BundleResource"
+    - Set Copy to output directory to "Always Copy"
+- Open Info.plist
+    - Click on the Source tab
+    - Add a new property and select "Fonts provided by application" from the dropdown list
+    - Add a new item to the array called FontAwesome.ttf
+    
+####Using it
+Once the library and font have been added, using FontAwesome is very simple. There is one static method `.Font(int size)` that returns the `UIFont` class with the specified size. Each icon is listed as a static method. The CSS class of `fa-lock` would become `FontAwesome.FALock`.
+
+```csharp
+using FontAwesomeXamarin;
+
+UILabel lockLabel = new UILabel (new RectangleF (0, 30, 320, 100)) {
+    Font = FontAwesome.Font(100), //100 is the font size we want to use
+    Text = FontAwesome.FALock, //Here's we're using the lock icon "fa-lock"
+    TextColor = UIColor.White,
+    TextAlignment = UITextAlignment.Center
+};
+```
+
+Because FontAwesome is a font based icon library we can set the text color and size in the same way we would for any other regular font.
