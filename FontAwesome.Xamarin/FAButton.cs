086286b4870903f4d02d15467f63bb7a07b4c36d
Neil Kennedy <neil.p.kennedy@hotmail.com>
2014-04-01 23:23:38 +0100

    Added an FAButton class. A subclass of UIButton

---
+++ FontAwesome.Xamarin/FAButton.cs
@@ -0,0 +1,72 @@
+﻿//
+// FAButton.cs
+//
+// Author:
+//       Neil Kennedy <neil.p.kennedy@outlook.com>
+//
+// Copyright (c) 2014 neilkennedy
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using MonoTouch.UIKit;
+
+namespace FontAwesomeXamarin
+{
+	public class FAButton : UIButton
+	{
+		/// <summary>
+		/// Gets or sets the icon for the button. Should be from <see cref="FontAwesome.Xamarin.FontAwesome"/>.
+		/// </summary>
+		/// <value>The icon.</value>
+		public string Icon {
+			get {
+				return this.Title (UIControlState.Normal);
+			}
+			set {
+				this.SetTitle (value, UIControlState.Normal);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the size of the icon
+		/// </summary>
+		/// <value>The size of the icon.</value>
+		public float IconSize{
+			get{ return this.Font.PointSize; }
+			set{
+				this.Font = FontAwesome.Font (value);
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FontAwesomeXamarin.FAButton"/> class.
+		/// This class extends UIButton. It does set set a default Frame. You must do this yourself
+		/// </summary>
+		/// <param name="icon">Icon.</param>
+		/// <param name="fontColor">Font color.</param>
+		/// <param name="iconSize">Icon size.</param>
+		public FAButton (string icon, UIColor fontColor, float iconSize = 20) : base(UIButtonType.System)
+		{
+			Icon = icon;
+			IconSize = iconSize;
+			this.SetTitleColor (fontColor, UIControlState.Normal);
+			this.SetTitleColor (fontColor.ColorWithAlpha(100), UIControlState.Highlighted);
+		}
+	}
+}
--- readme.markdown
+++ readme.markdown
@@ -21,10 +21,10 @@ Once the library and font have been added, using FontAwesome is very simple. The
 using FontAwesomeXamarin;
 
 UILabel lockLabel = new UILabel (new RectangleF (0, 30, 320, 100)) {
-    Font = FontAwesome.Font(100), //100 is the font size we want to use
-    Text = FontAwesome.FALock, //Here we're using the lock icon "fa-lock"
-    TextColor = UIColor.White,
-    TextAlignment = UITextAlignment.Center
+  Font = FontAwesome.Font(100), //100 is the font size we want to use
+  Text = FontAwesome.FALock, //Here we're using the lock icon "fa-lock"
+  TextColor = UIColor.White,
+  TextAlignment = UITextAlignment.Center
 };
 ```
 ![Icon in a UILabel](https://raw.github.com/neilkennedy/FontAwesome.Xamarin/master/images/icon.png)
@@ -36,14 +36,23 @@ This will create a `UIBarButtonItem` with a FontAwesome icon that can be used in
 
 ```csharp
 NavigationItem.RightBarButtonItem = new FABarButtonItem (FontAwesome.FATrashO, UIColor.White, delegate {
-    //Click event handler
+  //Click event handler
 });
 ```
 ![Navigation Bar Icon](https://raw.github.com/neilkennedy/FontAwesome.Xamarin/master/images/navigation_icon.png)
 
 ```csharp
 NavigationItem.RightBarButtonItem = new FABarButtonItem (FontAwesome.FAGithub, "Github", UIColor.White, delegate {
-    //Click event handler
+  //Click event handler
 });
 ```
 ![Navigation Bar Icon with Title](https://raw.github.com/neilkennedy/FontAwesome.Xamarin/master/images/navigation_icon_title.png)
+
+###FAButton.cs
+This is an extension of `UIButton`. It simply sets the font to `FontAwesome.Font()` and contains easy access methods to the `Icon` and `IconSize`.
+
+```csharp
+var deleteBtn = new FAButton (FontAwesome.FATrashO, UIColor.White, 20) {
+  Frame = new RectangleF (0 , 0, 25, 25)
+};
+```
